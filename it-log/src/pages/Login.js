8e072bb426eea3cc64613d9659638721fde9deee
Ghos13 <robocopsoo095@gmail.com>
2025-05-21 11:30:58 +0600

    12

--- it-log/src/pages/Login.js
+++ it-log/src/pages/Login.js
@@ -3,19 +3,29 @@ import React from "react";
 class Login extends React.Component {
   render() {
     return (
-      <main>
+      <main className="login-section">
         <div className="container">
           <form className="login-form">
-            <input className="login-name" type="text" name="name" id="name" />
+            <input
+              className="login-name"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Username"
+            />
             <input
               className="login-password"
               type="password"
               placeholder="Password"
             />
+            <button type="submit" className="login-button">
+              Log In
+            </button>
           </form>
         </div>
       </main>
     );
   }
 }
+
 export default Login;
